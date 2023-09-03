ba76508b5af29c2b2fd9c0fdef67e3737489333b
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-09-03 15:48:02 -0300

    CanDeactivate class 2

--- src/app/shared/guards/can-deactive.guard.ts
+++ src/app/shared/guards/can-deactive.guard.ts
@@ -2,5 +2,9 @@ import { CanDeactivateFn } from '@angular/router';
 import { AccountComponent } from '../account/account.component'
 
 export const canDeactiveGuard: CanDeactivateFn<AccountComponent> = (component, currentRoute, currentState, nextState) => {
+  console.log(currentRoute)
+  console.log(currentState)
+  console.log(nextState)
+  
   return component.exit();
 };
